360758aded6035cb0095d1b1cc8e36c58c37b616
Adyaraka Sholehudin Syahputra <rakatrizyy@gmail.com>
2026-08-26 23:22:20 +0700

    update

---
+++ src/app/array.jsx
@@ -0,0 +1,9 @@
+var hobbies = ["coding", "cycling", "climbing", "skateboarding"] 
+
+console.log(hobbies) // [ 'coding', 'cycling', 'climbing', 'skateboarding' ] 
+console.log(hobbies.length) // 4 
+console.log(hobbies[0]) // coding 
+console.log(hobbies[2]) // climbing
+// Mengakses elemen terakhir dari array 
+
+console.log(hobbies[hobbies.length -1])  // skateboarding 
